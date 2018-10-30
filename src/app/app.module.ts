70573296d6fe45d8e8a99fb263edc1111ba49065
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-10-30 20:44:02 -0300

    Custom preloading strategies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules, PreloadingStrategy, Route } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
 import { MailModule } from './mail/mail.module';
 
 import { AppComponent } from './app.component';
 import { SingletonService } from './mail/singleton.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+export class CustomPreload implements PreloadingStrategy {
+  preload(route: Route, fn: () => Observable<any>): Observable<any> {
+    return route.data && route.data.preload ? fn() : Observable.of(null);
+  }
+}
 
 export const ROUTES: Routes = [
-  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+  { path: 'dashboard', data: { preload: false }, loadChildren: './dashboard/dashboard.module#DashboardModule' },
   { path: '**', redirectTo: 'folder/inbox' }
 ];
 
@@ -21,10 +29,11 @@ export const ROUTES: Routes = [
     BrowserModule,
     HttpModule,
     MailModule,
-    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules, enableTracing: false })
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: CustomPreload, enableTracing: false })
   ],
   providers: [
-    SingletonService
+    SingletonService,
+    CustomPreload
   ],
   bootstrap: [
     AppComponent
